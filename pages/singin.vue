e58951a32867bbc628732833e4a62fd6674b4202
katsuhide.tsuruta <katsuhide.tsuruta@nulab.com>
2020-06-04 17:02:47 +0900

    feat authentication

---
+++ pages/singin.vue
@@ -0,0 +1,47 @@
+<template lang="pug">
+    amplify-sign-out(
+      v-if="isLoggedIn"
+    )
+    amplify-authenticator(
+      v-else
+      :authConfig="authConfig"
+    )
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      isLoggedIn: false,
+      authConfig: {
+        usernameAttributes: 'My user name',
+        signUpConfig: {
+          header: 'My Customized Sign Up!',
+          hideAllDefaults: true,
+          defaultCountryCode: '1',
+          signUpFields: [
+            {
+              label: 'My user name',
+              key: 'username',
+              required: true,
+              displayOrder: 1,
+              type: 'string'
+            },
+            {
+              label: 'Password',
+              key: 'password',
+              required: true,
+              displayOrder: 2,
+              type: 'password'
+            }
+          ]
+        }
+      }
+    }
+  },
+  async mounted() {
+    const currentUserInfo = await this.$Amplify.Auth.currentUserInfo()
+    this.isLoggedIn = Boolean(currentUserInfo)
+  }
+}
+</script>
